d4bd8d91f4498f52ad18b1a52f181580c7342825
JonasMoura-J <jonasmourat9090@gmail.com>
2021-03-15 11:59:21 -0300

    Adicionando Test exemplo de acesso ao textField

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,15 +1,29 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainTest {
+public class MainTest extends Main{
 	
 	@Test
-	public void test() {
+	public void acessoTest() {
 		WebDriver wd = new ChromeDriver();
 		wd.get("https://www.google.com.br/");
 		Assert.assertEquals("Google", wd.getTitle());
+		wd.quit();
+	}
+	
+	@Test
+	public void textFieldTest() {
+		WebDriver wd = new ChromeDriver();
+		wd.get("https://www.google.com.br/");
+		wd.findElement(By.name("q")).sendKeys("Teste escrita");
+		
+		//testando se obteve sucesso
+		String valorInput = wd.findElement(By.name("q")).getAttribute("value");
+		Assert.assertEquals("Teste escrita", valorInput);
+		
 		wd.quit();
 	}
 }
