c9e7a687007f9f72ac89c59e6e022ba891eb6f64
JonasMoura-J <jonasmourat9090@gmail.com>
2021-03-28 12:03:23 -0300

    adicionado teste com pop ups onde não se tem um id referenciando a janela

--- src/test/java/Tests/TestPopUp.java
+++ src/test/java/Tests/TestPopUp.java
@@ -25,4 +25,25 @@ public class TestPopUp {
 		
 		wd.quit();
 	}
+	
+	@Test
+	public void deveInteragirComPopUpHard() {
+		//caso em que o pop up não tem um identificador para fazer o switch de janelas
+		WebDriver wd = new ChromeDriver();
+		wd.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+		wd.findElement(By.id("buttonPopUpHard")).click();
+		
+		//dessa forma, tem como acessar o "id" de cada janela disponível na visualização. 
+		//Resolve o problema de não se ter um identificador direto
+		System.out.println(wd.getWindowHandle());
+		System.out.println(wd.getWindowHandles());	
+		wd.switchTo().window(wd.getWindowHandles().toArray()[1].toString());
+		wd.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		
+		wd.switchTo().window(wd.getWindowHandles().toArray()[0].toString());
+		wd.findElement(By.tagName("textarea")).sendKeys("Deu certo!");
+		
+		wd.quit();
+	}
 }
